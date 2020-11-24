e31bd1556941adc1cecf42a82eefedb6bba9ebc2
drusso <drusso@red.software.systems>
2020-11-24 16:28:47 +0100

    Add HAL formatting using EntityModel

--- src/main/java/systems/software/red/springrest/controller/EmployeeController.java
+++ src/main/java/systems/software/red/springrest/controller/EmployeeController.java
@@ -1,11 +1,17 @@
 package systems.software.red.springrest.controller;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import systems.software.red.springrest.error.EmployeeNotFoundException;
 import systems.software.red.springrest.repository.EmployeeRepository;
 import systems.software.red.springrest.entity.Employee;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -16,13 +22,29 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all(){
-        return employeeRepository.findAll();
+    CollectionModel<EntityModel<Employee>> all(){
+        List<EntityModel<Employee>> employees = employeeRepository.findAll().stream()
+            .map(employee ->
+                // Creo l'EntityModel per ogni Employee estratto dalla query
+                EntityModel.of(employee,
+                    linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                    linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+
+                )
+            ).collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable long id){
-        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee,
+            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+            );
     }
 
     @PostMapping("/employees")
