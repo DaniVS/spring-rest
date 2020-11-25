a7371b4bf909a60d11acde6a2afaf5126a74f6ee
drusso <drusso@red.software.systems>
2020-11-25 15:05:20 +0100

    Supporting changes to the API

--- src/main/java/systems/software/red/springrest/entity/Employee.java
+++ src/main/java/systems/software/red/springrest/entity/Employee.java
@@ -14,14 +14,26 @@ import java.util.Objects;
 public class Employee {
 
     private @Id @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String role;
 
     public Employee(){}
 
-    public Employee(String name, String role){
-        this.name = name;
+    public Employee(String firstName, String lastName, String role){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
+    }
+
+    public String getName(){
+        return this.firstName + " " + this.lastName;
+    }
+
+    public void setName(String name){
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 
     @Override
@@ -31,17 +43,23 @@ public class Employee {
 
         Employee employee = (Employee) obj;
         return Objects.equals(this.id, employee.id)
-            && Objects.equals(this.name, employee.name)
+            && Objects.equals(this.firstName, employee.firstName)
+            && Objects.equals(this.lastName, employee.lastName)
             && Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" +
+                "id=" + this.id +
+                ", firstName='" + this.firstName + '\'' +
+                ", lastName='" + this.lastName + '\'' +
+                ", role='" + this.role + '\'' +
+            '}';
     }
 }
